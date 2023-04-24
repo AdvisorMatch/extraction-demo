1944f6acc99d91f7cbd35cf11af84bb09e55c085
pontakornth <pontakorn61@gmail.com>
2023-04-24 09:04:37 +0700

    Addd link and name

--- extraction_app/urls.py
+++ extraction_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("extract/", views.extract),
-    path("list/", views.list_works)
+    path("extract/", views.extract, name='extract'),
+    path("", views.list_works, name='work_list'),
+    path("<int:id>/", views.work_detail, name='work_detail'),
 ]
